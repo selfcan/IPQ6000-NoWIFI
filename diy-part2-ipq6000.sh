1d3f079e8d9f9b29b6846e753a5078f18e98a6b5
selfcan <99958756+selfcan@users.noreply.github.com>
2024-01-08 12:47:27 +0800

    Update diy-part2-ipq6000.sh

--- diy-part2-ipq6000.sh
+++ diy-part2-ipq6000.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
 
 # Modify default Timezone
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
